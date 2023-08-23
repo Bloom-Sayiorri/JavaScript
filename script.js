cbf6c1c4aa0042401c583e727b3320390ecb9481
Bloom <bloomtauta@gmail.com>
2023-08-23 20:33:35 +0300

    html

---
+++ script.js
@@ -0,0 +1,10 @@
+const search = document.quetySelector("#search")
+const twitter = document.querySelector("#twitter")
+const whatsapp = document.querySelector("#whatsapp")
+const instagram = document.querySelector("#instagram")
+
+
+function handleSearch(){
+
+}
+search.addEventListener("onchange", handleSearch);
