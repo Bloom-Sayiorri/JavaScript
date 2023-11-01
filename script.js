81fc14d2441f6875700b3581fd28aadbc84ce209
+ <+>
2023-11-01 13:25:37 +0300

    changed folder name

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const search = document.quetySelector("#search")
 const twitter = document.querySelector("#twitter")
 const whatsapp = document.querySelector("#whatsapp")
 const instagram = document.querySelector("#instagram")
+const form = document.querySelector("form")
 
-
-function handleSearch(){
-
+function handleClick(e) {
+    const search = document.quesrySelector("#search_button")
+    search.addEventListener("click", function() {
+        handleSubmit()
+    })
 }
-search.addEventListener("onchange", handleSearch);
+
+function handleSubmit(e) {
+    e.preventDefault()
+    
+}
+
